49703571586cfdd7ecb43cd472fe44e91441a376
Leena Mandula <leleena@cisco.com>
2024-09-16 12:46:39 +0530

    intro to python

---
+++ folder1/Employee.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self,name,address,code,salary):
+        self.name=name
+        self.address=address
+        self.code=code
+        self.salary=salary 
+
+    def __str__(self):
+          return f'{self.name},{self.address},{self.code},{self.salary}'
+
+
+mahesh=Employee('Mahesh','pune','pufds90',1000)
+print(mahesh)
+
---
+++ folder1/sum.py
@@ -0,0 +1,5 @@
+def sum(first,second):
+     return first+second
+
+s1=sum(10,20)
+print(s1)
